828130a5c08a6a66ebd83cb0d7cc5f0f8bb4db4f
BraydenUA <babruins@ualberta.ca>
2024-03-06 13:51:57 -0700

    Added image upload and display to event browsing and creation.

--- its-a-feature-not-a-bug/app/src/main/java/com/example/its_a_feature_not_a_bug/Event.java
+++ its-a-feature-not-a-bug/app/src/main/java/com/example/its_a_feature_not_a_bug/Event.java
@@ -1,25 +1,32 @@
 package com.example.its_a_feature_not_a_bug;
 
+import android.net.Uri;
 import android.widget.ImageView;
 
+import java.net.URI;
 import java.util.Date;
 
 public class Event {
-    private int imageID; // event poster
+    private Uri imageId; // event poster
     private String title; // name of the event
     private String host; // user that created the event
     private Date date; // date of the event
     private String description; // short description of the event posted by the host
 
-    public Event(String title, String host, Date date, String description) {
+    public Event(Uri imageId, String title, String host, Date date, String description) {
+        this.imageId = imageId;
         this.title = title;
         this.host = host;
         this.date = date;
         this.description = description;
     }
 
-    public int getPoster() {
-        return this.imageID;
+    public Uri getImageId() {
+        return this.imageId;
+    }
+
+    public void setImageId(Uri imageId) {
+        this.imageId = imageId;
     }
 
     public String getTitle() {
