901d212302ba0991f95f996a475b94fd90dbfade
BraydenUA <babruins@ualberta.ca>
2024-03-30 15:11:14 -0600

    Brayden
    Refactored AddEventFragment and added functionality to scan QR code with camera

---
+++ its-a-feature-not-a-bug/app/src/main/java/com/example/its_a_feature_not_a_bug/QRCodeScanner.java
@@ -0,0 +1,10 @@
+package com.example.its_a_feature_not_a_bug;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class QRCodeScanner extends CaptureActivity {
+}
