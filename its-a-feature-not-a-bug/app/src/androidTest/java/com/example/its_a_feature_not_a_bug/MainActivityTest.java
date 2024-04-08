3f244830fed747ee471aecd735e27b2b7350f71c
Jingyang Xu <jingyan6@ualberta.ca>
2024-04-08 15:34:50 -0600

    Update MainActivityTest.java

--- its-a-feature-not-a-bug/app/src/androidTest/java/com/example/its_a_feature_not_a_bug/MainActivityTest.java
+++ its-a-feature-not-a-bug/app/src/androidTest/java/com/example/its_a_feature_not_a_bug/MainActivityTest.java
@@ -1,4 +1,3 @@
-
 package com.example.its_a_feature_not_a_bug;
 
 import static androidx.test.espresso.Espresso.onData;
@@ -18,9 +17,6 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 
-import android.support.test.InstrumentationRegistry;
-import android.support.test.rule.GrantPermissionRule;
-
 import androidx.core.widget.ListViewAutoScrollHelper;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
@@ -29,10 +25,6 @@ import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-import androidx.test.uiautomator.UiDevice;
-import androidx.test.uiautomator.UiObject;
-import androidx.test.uiautomator.UiObjectNotFoundException;
-import androidx.test.uiautomator.UiSelector;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -59,11 +51,9 @@ public class MainActivityTest {
         onView(withText("OK")).perform(click());
         onView(withText("STUDYING")).check(matches(isDisplayed()));
     }
-    @Test
-    public void testDeleteEvent() {
 
     @Test
-    public void testRemoveEvent() {
+    public void testDeleteEvent() {
         // add an event
         // Start Activity
         ActivityScenario.launch(MainActivity.class);
@@ -89,7 +79,7 @@ public class MainActivityTest {
         onView(withText("CRYING")).check(doesNotExist());
     }
     @Test
-    public void testUpdateProfile(){
+    public void testUpdateProfile() {
         // Start Activity
         ActivityScenario.launch(MainActivity.class);
         // Click on Organizer button
@@ -97,30 +87,25 @@ public class MainActivityTest {
         // Click on Profile button
         onView(withId(R.id.button_profile)).perform(click());
         // Type "Jingyang" in the editTest
-        onView(withId(R.id.editTextFullName)).perform(ViewActions.typeText("Jingyang"));
+        onView(withId(R.id.editTextFullName)).perform(ViewActions.replaceText("Jingyang"));
         Espresso.closeSoftKeyboard();
-        // Press submit button buttonSubmit
+        // Press Submit buttonSubmit
         onView(withId(R.id.buttonSubmit)).perform(click());
+        // Sleep for 2 seconds
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // Click on Profile Button
+        onView(withId(R.id.button_profile)).perform(click());
+        // Check if editTextFullName is displaying "Jingyang"
         onView(withText("Jingyang")).check(matches(isDisplayed()));
+        //onView(withId(R.id.editTextFullName)).check()
+
+
     }
 
-        // Type "STUDYING" in the editText
-        onView(withId(R.id.edit_text_event_title)).perform(ViewActions.typeText("STUDYING"));
-        // Close soft keyboard
-        Espresso.closeSoftKeyboard();
-        onView(withText("OK")).perform(click());
-        onView(withText("STUDYING")).check(matches(isDisplayed()));
-
-        // delete the event
-        onData(anything())
-                .inAdapterView(withId(R.id.list_view_events_list))
-                .atPosition(0)
-                .perform(click());
-        onView(withId(R.id.deleteEventButton)).perform(click());
-        Espresso.closeSoftKeyboard();
-        onView(withText("STUDYING"))
-                .check(matches(not(isDisplayed())));
-    }
 }
 
 
